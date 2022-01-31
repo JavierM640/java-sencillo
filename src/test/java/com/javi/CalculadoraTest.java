9a781ffcf51f0ed20794a40f6a89dbc71a1be3b0
JavierM640 <fmaroje640@g.educaand.es>
2022-01-31 14:16:14 +0100

    pepe

--- src/test/java/com/javi/CalculadoraTest.java
+++ src/test/java/com/javi/CalculadoraTest.java
@@ -1,7 +1,6 @@
 package com.javi;
 
-import static
-org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,27 +8,27 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
-Calculadora calculadora;
+    Calculadora calculadora;
 
-@BeforeEach
-void setUp() {
-calculadora = new Calculadora();
-}
+    @BeforeEach
+    void setUp() {
+        calculadora = new Calculadora();
+    }
 
-@Test
-@DisplayName("Multiplicación sencilla debería ir")
-void testMultiplicar() {
-assertEquals(20, calculadora.multiplicar(4, 5),
-"Multiplicación normal debe funcionar");
+    @Test
+    @DisplayName("Multiplicación sencilla debería ir")
+    void testMultiplicar() {
+        assertEquals(20, calculadora.multiplicar(4, 5),
+                "Multiplicación normal debe funcionar");
 
-}
+    }
 
-@RepeatedTest(5)
-@DisplayName("Manejo del cero")
-void testMultiplicarPorCero() {
-assertEquals(0, calculadora.multiplicar(0, 5),
-"Multiplicar por cero debe ser cero");
-assertEquals(0, calculadora.multiplicar(5, 0),
-"Multiplicar por cero debe ser cero");
-}
+    @RepeatedTest(9)
+    @DisplayName("Manejo del cero")
+    void testMultiplicarPorCero() {
+        assertEquals(0, calculadora.multiplicar(0, 5),
+                "Multiplicar por cero debe ser cero");
+        assertEquals(0, calculadora.multiplicar(5, 0),
+                "Multiplicar por cero debe ser cero");
+    }
 }
